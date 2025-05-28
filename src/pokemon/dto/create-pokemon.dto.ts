78e15b423d5d6c1afafd4e2ce259473640d1f601
Jhonny <jhonnycgarcia@gmail.com>
2025-05-27 23:40:51 -0500

    POST - recibir y validar la data

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, IsNotEmpty, Min, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
+  name: string;
+}
